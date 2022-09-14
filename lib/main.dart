dfd5e512eea6c4567cc67bb790fff058fc0874d9
nishantnz <nishant.z@somaiya.edu>
2022-09-14 20:34:17 +0530

    reformatiing the code

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
-import 'package:expense_tracker/transaction.dart';
+import 'package:expense_tracker/models/transaction.dart';
+import 'package:expense_tracker/widgets/new_transaction.dart';
+import 'package:expense_tracker/widgets/transactions_list.dart';
+import 'package:expense_tracker/widgets/user_transations.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -8,20 +11,6 @@ void main() {
 
 class MyApp extends StatelessWidget {
   MyApp({super.key});
-  final List<Transactions> transaction = [
-    Transactions(
-      id: 't1',
-      title: "new Shoes",
-      amount: 12,
-      date: DateTime.now(),
-    ),
-    Transactions(
-      id: 't2',
-      title: "new car",
-      amount: 1200,
-      date: DateTime.now(),
-    ),
-  ];
 
   @override
   Widget build(BuildContext context) {
@@ -30,55 +19,20 @@ class MyApp extends StatelessWidget {
       appBar: AppBar(
         title: Text("Expense Tracker"),
       ),
-      body: Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-        Container(
-          width: double.infinity,
-          child: Card(
-            child: Text("chart"),
-            elevation: 10.76,
-            color: Colors.blue,
-          ),
-        ),
-        Column(
-          children: transaction.map((tx) {
-            return Card(
-              child: Row(
-                children: [
-                  Container(
-                    padding: EdgeInsets.all(10),
-                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-                    decoration: BoxDecoration(
-                      color: Colors.blue,
-                      border: Border.all(color: Colors.black, width: 2),
-                    ),
-                    child: Text(
-                      tx.amount.toString(),
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                      ),
-                    ),
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        tx.title,
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 15),
-                      ),
-                      Text(
-                        DateFormat.yMMMMEEEEd().add_Hm().format(tx.date),
-                        style: TextStyle(color: Colors.grey),
-                      ),
-                    ],
-                  )
-                ],
+      body: Column(
+          //mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              width: double.infinity,
+              child: Card(
+                child: Text("chart"),
+                elevation: 10.76,
+                color: Colors.blue,
               ),
-            );
-          }).toList(),
-        ),
-      ]),
+            ),
+            UserTransaction()
+          ]),
     ));
   }
 }
