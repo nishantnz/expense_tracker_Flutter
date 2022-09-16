93445f557407c049feeac6183b857e7bb67e9c4d
nishantnz <nishant.z@somaiya.edu>
2022-09-16 19:08:34 +0530

    made it stateful and added some checks

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -2,12 +2,30 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
-class NewTransaction extends StatelessWidget {
+class NewTransaction extends StatefulWidget {
   final Function newTransactionHandler;
   NewTransaction(this.newTransactionHandler, {super.key});
 
+  @override
+  State<NewTransaction> createState() => _NewTransactionState();
+}
+
+class _NewTransactionState extends State<NewTransaction> {
   final titleController = TextEditingController();
+
   final amountController = TextEditingController();
+
+  void submitData() {
+    final enteredTitle = titleController.text;
+    final enteredAmount = double.parse(amountController.text);
+    if (enteredTitle.isEmpty || enteredAmount <= 0) {
+      return;
+    }
+    widget.newTransactionHandler(
+        titleController.text, double.parse(amountController.text));
+//pops out the topmost screen/page that is displayed ...obviously when you submit the data
+    Navigator.of(context).pop();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,20 +35,17 @@ class NewTransaction extends StatelessWidget {
         padding: EdgeInsets.all(10),
         child: Column(children: [
           TextField(
-            autofocus: true,
             decoration: InputDecoration(labelText: "Title"),
             controller: titleController,
+            onSubmitted: (_) => submitData(),
           ),
           TextField(
             decoration: InputDecoration(labelText: "Amount"),
             controller: amountController,
+            keyboardType: TextInputType.number,
+            onSubmitted: (_) => submitData(),
           ),
-          TextButton(
-              onPressed: () => {
-                    newTransactionHandler(titleController.text,
-                        double.parse(amountController.text))
-                  },
-              child: Text("Add Transaction"))
+          TextButton(onPressed: submitData, child: Text("Add Transaction"))
         ]),
       ),
     );
