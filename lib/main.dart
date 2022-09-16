59074a413b642f52734f2edef91027a370396ff4
nishantnz <nishant.z@somaiya.edu>
2022-09-16 19:07:03 +0530

    added:theme,icons,floating action buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:expense_tracker/models/transaction.dart';
 import 'package:expense_tracker/widgets/new_transaction.dart';
 import 'package:expense_tracker/widgets/transactions_list.dart';
-import 'package:expense_tracker/widgets/user_transations.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -9,15 +8,77 @@ void main() {
   runApp(MyApp());
 }
 
+/*used this link
+https://www.fluttercampus.com/guide/69/how-to-solve-no-mediaquery-widget-found-error-exception-flutter/
+for mediaquery error
+*/
 class MyApp extends StatelessWidget {
-  MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
+      title: 'Expense Tracker',
+      home: MyHomePage(),
+      theme: ThemeData(
+        primarySwatch: Colors.cyan,
+        fontFamily: 'OpenSans',
+        /*  if you want to apply default theme to all the app bar in your
+        appBarTheme: AppBarTheme(
+            titleTextStyle: TextStyle(
+                fontFamily: 'OpenSans',
+                fontSize: 20,
+                fontWeight: FontWeight.bold)),*/
+      ),
+      // darkTheme: ThemeData(primarySwatch: Colors.red),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<Transactions> _userTransactions = [];
+
+  void _addNewTransaction(String txTitle, double txAmount) {
+    final newTx = Transactions(
+      id: DateTime.now().toString(),
+      title: txTitle,
+      amount: txAmount,
+      date: DateTime.now(),
+    );
+    setState(() {
+      _userTransactions.add(newTx);
+    });
+  }
+
+  void _startAddNewTransactionModel(BuildContext ctx) {
+    showModalBottomSheet(
+        context: ctx,
+        builder: (_) {
+          return GestureDetector(
+            onTap: () {},
+            child: NewTransaction(_addNewTransaction),
+            behavior: HitTestBehavior.opaque,
+          );
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
         title: Text("Expense Tracker"),
+        titleTextStyle: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () => _startAddNewTransactionModel(context),
+          ),
+        ],
       ),
       body: SingleChildScrollView(
         child: Column(
@@ -29,12 +90,17 @@ class MyApp extends StatelessWidget {
                 child: Card(
                   child: Text("chart"),
                   elevation: 10.76,
-                  color: Colors.blue,
+                  //color: Colors.blue,
                 ),
               ),
-              UserTransaction()
+              TransactionList(_userTransactions),
             ]),
       ),
-    ));
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => _startAddNewTransactionModel(context),
+      ),
+    );
   }
 }
